f3134cf579d2f4358b3da3d0db209e4a289198e3
Sarruby <emilyfranklin.email@gmail.com>
2017-04-19 20:19:09 -0700

    Add status constants

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,6 +1,10 @@
 var constants = {
 	'CONSTANT_MESSAGE': 'hello world',
-	'NO_TOURNEY': 0
+	'NO_TOURNEY': 0,    // no tournament is set up
+	'INIT_TOURNEY': 1,  // user is setting tournament params
+	'SETUP_TOURNEY': 2, // players are joining tournament
+	'RUN_TOURNEY': 3,   // tournament is in progress
+	'CLOSE_TOURNEY': 4, // tournament is finished, perform clean/reset to none
 };
 
 module.exports = constants;
