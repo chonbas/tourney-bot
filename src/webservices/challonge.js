660c8b8caf83ed116d29d3013100a2c372ea89e9
Ben Krausz <bkrausz@stanford.edu>
2017-04-30 16:38:21 -0700

    added webservers/challonge

---
+++ src/webservices/challonge.js
@@ -0,0 +1,8 @@
+const challonge = require('challonge');
+const token = require('../../credentials').CHALLONGE_TOKEN;
+
+const client = challonge.createClient({
+apiKey: token
+});
+
+module.exports = client;
