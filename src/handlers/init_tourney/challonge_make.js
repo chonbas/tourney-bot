bc85a63c1d12602a957177f9b6b89ca7ee61cfb8
Sarruby <emilyfranklin.email@gmail.com>
2017-05-07 17:31:22 -0700

    fix challonge_make.js

--- src/handlers/init_tourney/challonge_make.js
+++ src/handlers/init_tourney/challonge_make.js
@@ -5,43 +5,43 @@ var client = require('../../webservices/challonge');
 
 //Generates random strings for the URL
 function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-    return result;
+	var result = '';
+	for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+	return result;
 }
 
 function processName(msg) {
 	Console.log('REGEX STUFF');
 	Console.log(msg);
-	name = msg.replace(/<@\d+>/i," ");
+	var name = msg.replace(/<@\d+>/i,' ');
 	Console.log(name);
-	return name
+	return name;
 }
 
 var makeChallongeTourney = (msg) => {
 	Console.log(msg.content);
-	t_name = processName(msg.content)
+	var t_name = processName(msg.content);
 	return new Promise((fulfill, reject) => {
 		// TODO: make channels for tournament
 		client.tournaments.create({
-		  	tournament: {
-		   		name: t_name,
-		    	url: randomString(9, '0123456789abcdefghijklmnopqrstuvwxyz'),
-		    	tournamentType: 'single elimination',
-		  	},
-		  	callback: (err, data) => {
-		  		Console.log('BEGINNING OF CALLBACK');
-			    if(err){
-			    	Console.log('ERROR HERE');
-			    	reject();
-			    }
-			    else{
-			    	Console.log(err,data)
-			    	Console.log('data 0 returned is: ' + data[0]);
-			    	Console.log('data 1 returned is: ' + data[1]);
+			tournament: {
+				name: t_name,
+				url: randomString(9, '0123456789abcdefghijklmnopqrstuvwxyz'),
+				tournamentType: 'single elimination',
+			},
+			callback: (err, data) => {
+				Console.log('BEGINNING OF CALLBACK');
+				if(err){
+					Console.log('ERROR HERE');
+					reject();
+				}
+				else{
+					Console.log(err,data);
+					Console.log('data 0 returned is: ' + data[0]);
+					Console.log('data 1 returned is: ' + data[1]);
 					var challonge_id = 'GARBAGE';
 					fulfill(challonge_id);
-			    }
+				}
 			}
 		});
 	});
