a86b4cdc91044e26c516a954a8c95484adfa9b5b
Alexei Bastidas <bastidas.alexei@gmail.com>
2017-05-12 15:21:36 -0700

    added db clearing on MONGO_DEBUG

--- src/tests/dbTests.js
+++ src/tests/dbTests.js
@@ -1,54 +1,9 @@
-const db = require('../webservices/mongodb');
-const Console = require('../util/console');
-const assert = require('assert');
-exports = {};
+// const db = require('../webservices/mongodb');
+// const Console = require('../util/console');
+// const assert = require('assert');
+// exports = {};
 
 
-function resetTourney(msg) {
-	db.deleteTournament('1234').then((data) =>{
-		Console.log(data);
-		Console.log(msg);
-	}).catch((err)=>{
-		Console.log(err);
-	});
-}
-
-exports.resetDB = () => {
-	resetTourney('bam');
-};
-
-exports.testTourneyCreation = () => {
-	db.createTournament('1234').then((data) =>{
-		Console.log(data);
-		resetTourney('Tourney Creation PASS');
-	}).catch((err) =>{
-		Console.log(err);
-		resetTourney('Tourney Creation FAIL');
-	});
-};
-
-exports.testTourneyChallongeSetGet = () => {
-	db.createTournament('1234').then((data) =>{
-		Console.log(data);
-		db.setChallongeID('1234', 'challonge').then((data) => {
-			Console.log(data);
-			db.getChallongeID('1234').then((data) => {
-				assert.equal(data, 'challonge');
-				Console.log(data);
-				resetTourney('TourneyChallongeSetGet PASS');
-			}).catch((err) => {
-				Console.log(err);
-				resetTourney('TourneyChallongeSetGet FAIL');
-			});
-		}).catch((err) => {
-			Console.log(err);
-			resetTourney('TourneyChallongeSetGet FAIL');
-		});
-	}).catch((err) =>{
-		resetTourney('TourneyChallongeSetGet FAIL');
-		Console.log(err);
-	});
-};
 
 
 // exports.testParticipantCreateGet = () => {
