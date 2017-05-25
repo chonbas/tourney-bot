0c88ee484d3838a98bea1c841de638fc2824b538
Alexei Bastidas <bastidas.alexei@gmail.com>
2017-05-24 18:18:53 -0700

    added new match+stage tourney schema

--- src/webservices/schemas/guildSchema.js
+++ src/webservices/schemas/guildSchema.js
@@ -6,7 +6,8 @@ var dispute_schema = new mongoose.Schema({
 	originator: String, //discord_id for originator
 	defendant: String,
 	type: Number,
-	additional_info: String
+	additional_info: String,
+	resolved: {type:Boolean, default:false}
 });
 
 var participant_schema = new mongoose.Schema({
@@ -51,6 +52,21 @@ var chat_state_schema = new mongoose.Schema({
 	type: mongoose.Schema.Types.Mixed
 });
 
+var match_schema = new mongoose.Schema({
+	team_1:{
+		win:{type:Boolean, default:null},
+		vote:{type:Boolean, default:false},
+		name:{type:String}
+	},
+	team_2:{
+		win:{type:Boolean, default:null},
+		vote:{type:Boolean, default:false},
+		name:{type:String}
+	},
+	team_2_vote:{type:Boolean, default:false},
+	winner: String
+});
+
 
 var guild_schema = new mongoose.Schema({
 	guild_id: {
@@ -67,8 +83,18 @@ var guild_schema = new mongoose.Schema({
 	tourney_sub_state: Number, //If tourney is RUNNING, specify cycle state
 	teams:  [team_schema],
 	channels: [channel_schema],
+	active_matches: [match_schema],
 	chat_state: [chat_state_schema],
 	disputes: [dispute_schema]
+});
+
+var staged_tourney = new mongoose.Schema({
+	name:String,
+	tournament_type:Number, //Use constants
+	open_signup: {type:Boolean, default:false},
+	ranked_by:Number, //Use constants
+	signup_cap:Number,//Max users
+	start_at:Date
 });
 
 var Team = mongoose.model('Team', team_schema);
