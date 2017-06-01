a5eab248efd6ba770fb2484b3c6af15325e6a0bd
Sarruby <emilyfranklin.email@gmail.com>
2017-05-31 17:23:36 -0700

    Added skeleton for Ben to check errorounseus messages
    It's a promise returning function that needs some meat.

---
+++ src/handlers/error_handler/handler.js
@@ -0,0 +1,16 @@
+//require things up here
+
+//eslint-disable-next-line
+var errhandle = (msg, tournament_status, channel_type) => {
+	return new Promise((fulfill, reject) => {
+		//TODO: check states
+		//TODO: give helpful error messages
+		fulfill(true);
+		//fulfill true if everyone's ok and the handlers should run_tourney
+		//like green light
+		//fulfill false stops handlers from tripping
+		reject(/*erros*/);
+	});
+};
+
+module.exports = errhandle;
