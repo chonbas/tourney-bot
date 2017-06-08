08a282f74cc2c5e74b5687c05c6f78d1d159b803
Alexei Bastidas <bastidas.alexei@gmail.com>
2017-06-08 09:43:22 -0700

    Revert "init parsing workaround error (#94)"
    This reverts commit 3755abd53bb79ffde1ce6b82c419581579212580.

--- src/util/parser_util.js
+++ src/util/parser_util.js
@@ -4,10 +4,10 @@ exports = {};
 
 exports.propToQuestion = (prop) =>{
 	if (prop === 'guild_id'){ return 'pass';}
-	if (prop === 'tourney_name'){ return 'NAME';}
-	if (prop === 'tournament_type'){ return 'T_TYPE';}
-	if (prop === 'teams'){ return 'TEAMS';}
-	if (prop === 'signup_cap'){ return 'SIGNUP_CAP';}
+	if (prop === 'name'){ return parser_constants.NAME;}
+	if (prop === 'tournament_type'){ return parser_constants.T_TYPE;}
+	if (prop === 'teams'){ return parser_constants.TEAMS;}
+	if (prop === 'signup_cap'){ return parser_constants.SIGNUP_CAP;}
 	return;
 };
 
