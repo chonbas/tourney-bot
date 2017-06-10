0f410606feed85c49f757aaf87aae74bde183326
Alexei Bastidas <bastidas.alexei@gmail.com>
2017-06-10 13:21:37 -0700

    added gcloud app.yaml file

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,4 +1,6 @@
-//
+const creds = require('../../credentials');
+
+
 var constants = {
 	'DEBUG': false, // DEBUG FLAG USED TO RUN TESTS
 	'MONGO_DEBUG': true, //MONGO DEBUG FLAG USED TO LOG ALL MONGO OPS
@@ -51,11 +53,10 @@ var constants = {
 	'PARTICIPANT_IN_TEAM':6,
 	'DISPUTE_EXISTS':7,
 	'TEAM_EXISTS':8,
-
+	// 'DATABASE_ADDRESS':	'mongodb://' + creds.MONGO_USER + ':' + creds.MONGO_PASS + '@ds113702.mlab.com:13702/tourneydb'// update to live when needeed
 	// other constants
 	'CONSTANT_MESSAGE': 'hello world',
-
-	'DATABASE_ADDRESS':	'mongodb://localhost/test' // update to live when needeed
+	'DATABASE_ADDRESS': 'mongodb://localhost/test'
 };
 //
 module.exports = constants;
