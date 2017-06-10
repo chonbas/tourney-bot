3a1d4d20bd898de8d8f46233852766197462b891
Sarruby <emilyfranklin.email@gmail.com>
2017-06-10 00:25:57 -0700

    Added wipe-msg-db to cleardb command
    The message database was separate from everything, so
    now I've added it to the cleardb command to make sure
    the back end is totally cleaned up by cleardb. :-)

--- src/util/wipeDB.js
+++ src/util/wipeDB.js
@@ -1,10 +1,14 @@
 const Console = require('console');
 var db = require('../webservices/mongodb.js');
+var msg_db = require('../webservices/mongodb_messages.js');
 var Challonge = require('../webservices/challonge');
 
 var wipeDB = () => {
 	return new Promise((fulfill, reject) => {
 		db.clearDB()
+		.then(() => {
+			return msg_db.clearDB();
+		})
 		.then(() => {
 			return Challonge.removeAllTourneys();
 		})
