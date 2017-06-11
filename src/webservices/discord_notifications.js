75cdf94af20636f4c3abc86420b6656aeddc017f
Sarruby <emilyfranklin.email@gmail.com>
2017-06-10 18:30:57 -0700

    Stopped missing createDM from killing bot

--- src/webservices/discord_notifications.js
+++ src/webservices/discord_notifications.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line
 const Console = require('../util/console');
+const constants = require('./discord_util/constants');
 
 var exports = {};
 
@@ -10,8 +11,14 @@ var dmPlayer = (user_obj, msg_txt) => {
 	if(user_obj.dmChannel){
 		return user_obj.dmChannel.send(msg_txt);
 	}
-	return user_obj.createDM()
-	.then((channel) => {return channel.send(msg_txt);});
+	if(user_obj.createDM){
+		return user_obj.createDM()
+		.then((channel) => {return channel.send(msg_txt);});
+	}
+	return Promise.reject({
+		user: user_obj,
+		err: 'ATTEMPTED TO SEND MESSAGE TO USER ABOVE BUT USER DID NOT HAVE CREATE_DMCHANNEL FXN'
+	});
 };
 
 /*
@@ -30,6 +37,16 @@ returns a Promise<message>
 exports.notifyPlayer = (guild_obj, player_id, message_txt) => {
 	var player_obj = guild_obj.members.get(player_id);
 	return dmPlayer(player_obj, message_txt);
+};
+
+/*
+returns a Promise<Array<message>>
+*/
+exports.notifyAllPlayers = (guild_obj, message_txt) => {
+	var role_obj = guild_obj.roles.find('name', constants.GENERAL_ROLE_NAME);
+	return Promise.all(role_obj.members.map(user_obj => {
+		return dmPlayer(user_obj, message_txt);
+	}));
 };
 
 module.exports = exports;
