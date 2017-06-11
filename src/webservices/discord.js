f55d4165afac465a55a7cb8ff26a287aea2d8523
Gordonski <gegdean@gmail.com>
2017-06-11 12:13:07 -0700

    Made match confirmations reflect the role_id (aka team name)

--- src/webservices/discord.js
+++ src/webservices/discord.js
@@ -332,7 +332,7 @@ exports.runInitMatchChannel = (guild, role_ids, match_number, ref_id) => {
 exports.sendConfirmMatchReport = (channel, reporter_user_id, confirmer_role_id, report) => {
 	return util.sendConfirmMessage(
 		channel,
-		str_gen.stub(`Hey <@${confirmer_role_id}>], <@${reporter_user_id}> says ${report.txt}. Is that right?`, 'join team confirm'),
+		str_gen.stub(`Hey <@&${confirmer_role_id}>], <@${reporter_user_id}> says ${report.txt}. Is that right?`, 'join team confirm'),
 		discord_constants.MATCH_REPORT_MESSAGE,
 		reporter_user_id,
 		confirmer_role_id,
